421f8e9b201dd8d37cd81d38bf571c137bdc3b3c
Toni1357 <a.capber@assomptioncolmar.fr>
2025-09-11 22:50:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -45,15 +45,16 @@ function formatCompact(n) {
   const abs = Math.abs(n);
   const nf1 = new Intl.NumberFormat('en-US', { maximumFractionDigits: 2 });
 
-  if (abs >= 1e18) return `\`${nf1.format(n / 1e18)}QT\``;
-  if (abs >= 1e15) return `\`${nf1.format(n / 1e15)}Q\``;
-  if (abs >= 1e12) return `\`${nf1.format(n / 1e12)}T\``;
-  if (abs >= 1e9)  return `\`${nf1.format(n / 1e9)}B\``;
-  if (abs >= 1e6)  return `\`${nf1.format(n / 1e6)}M\``;
-  if (abs >= 1e3)  return `\`${nf1.format(n / 1e3)}k\``;
+  if (abs >= 1e18) return nf1.format(n / 1e18) + 'QT';
+  if (abs >= 1e15) return nf1.format(n / 1e15) + 'Q';
+  if (abs >= 1e12) return nf1.format(n / 1e12) + 'T';
+  if (abs >= 1e9)  return nf1.format(n / 1e9) + 'B';
+  if (abs >= 1e6)  return nf1.format(n / 1e6) + 'M';
+  if (abs >= 1e3)  return nf1.format(n / 1e3) + 'k';
 
-  return `\`${nf1.format(n)}\``;
+  return nf1.format(n);
 }
+
 
 
 function totalAutoClicksPerSecond() {
