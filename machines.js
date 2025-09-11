c0380bda27074c3ecabb6d5ace43f4f0d90e8901
Toni1357 <a.capber@assomptioncolmar.fr>
2025-09-11 21:30:29 +0200

    Create machines.js

---
+++ machines.js
@@ -0,0 +1,13 @@
+// machines.js
+export const machines = [
+  { level: 1, title: "Machine N1 (+5/s)", base: 50, key: "machinesLevel1" },
+  { level: 2, title: "Machine N2 (+10/s)", base: 100, key: "machinesLevel2" },
+  { level: 3, title: "Machine N3 (+25/s)", base: 250, key: "machinesLevel3" },
+  { level: 4, title: "Machine N4 (+50/s)", base: 500, key: "machinesLevel4" },
+  { level: 5, title: "Machine N5 (+100/s)", base: 1_000, key: "machinesLevel5" },
+  { level: 6, title: "Machine N6 (+250/s)", base: 10_000, key: "machinesLevel6" },
+  { level: 7, title: "Machine N7 (+500/s)", base: 50_000, key: "machinesLevel7" },
+  { level: 8, title: "Machine N8 (+1000/s)", base: 100_000, key: "machinesLevel8" },
+  { level: 9, title: "Machine N9 (+2500/s)", base: 500_000, key: "machinesLevel9" },
+  { level: 10, title: "Machine N10 (+5000/s)", base: 1_000_000, key: "machinesLevel10" },
+];
