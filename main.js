8775b4fd29331693a5beb554b7912977852bf721
Toni_4819 <132385090+Toni4819@users.noreply.github.com>
2026-02-02 22:23:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -162,7 +162,7 @@ function renderMain() {
     getShopBoostFactor();
 
   els.tapBtn.textContent = `👇 Tapper (+${realPerClick.toFixed(2)})`;
-  els.versionText.textContent = `Toni’s Studios – v2.6`;
+  els.versionText.textContent = `Toni’s Studios – v2.7`;
 
 
   if (els.boostValue) {
